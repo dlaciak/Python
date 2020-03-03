a59d4c60a67f2f7fbd5349598728e4ef65032693
dlaciak <dawidlaciak@gmail.com>
2020-03-03 21:24:33 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Elo mordeczko")
+
+#uruchamianie z konsoli komenda     python \ścieżka-do pliku
+#python C:\Users\dawid\Desktop\Python\FlyNerd2\'Uruchamianie z konsoli'.py   nazwa pliku w ' gdy są spacje
