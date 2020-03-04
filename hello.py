35d389264eb52cf410be0952323fa05cc8b084cf
dlaciak <61757005+dlaciak@users.noreply.github.com>
2020-03-04 19:12:52 +0100

    Delete hello.py

--- hello.py
+++
@@ -1,4 +0,0 @@
-print("Elo mordeczko")
-
-#uruchamianie z konsoli komenda     python \ścieżka-do pliku
-#python C:\Users\dawid\Desktop\Python\FlyNerd2\'Uruchamianie z konsoli'.py   nazwa pliku w ' gdy są spacje
